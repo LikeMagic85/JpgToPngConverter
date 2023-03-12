08952796aebdb8b3733fe08426af537ac019b287
LikeMagic85 <like-magic@yandex.ru>
2023-03-12 11:27:25 +0300

    Initial commit

---
+++ app/src/main/java/com/like_magic/jpgtopngconverter/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.like_magic.jpgtopngconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
